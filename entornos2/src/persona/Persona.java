15ab046de1d4db3e20e7984106fc59ca37a82351
Laura Jiménez Villalba <a_laura.jimenez.villalba@iespablopicasso.es>
2023-02-03 13:52:55 -0100

    Creado persona

---
+++ entornos2/src/persona/Persona.java
@@ -0,0 +1,18 @@
+package persona;
+
+public class Persona {
+  private String nombre;
+  private String apellidos;
+  private int edad;
+  
+  public Persona(String nombre, String apellidos, int edad) {
+    this.nombre = nombre;
+    this.apellidos = apellidos;
+    this.edad = edad;
+  }
+  
+  public void cumpleanios() {
+    this.edad += 1;
+    System.out.println(this.edad);
+}
+}
